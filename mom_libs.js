e01d9405c962cdaedcbb0c6fd1e11a1223eebd5e
Thomas Williams <thomas.williams@live.co.uk>
2017-11-24 18:25:30 +0100

    working basic madlibs game

--- mom_libs.js
+++ mom_libs.js
@@ -4,7 +4,20 @@ const submitButton = document.getElementById('text-submit');
 
 const newText = document.getElementById('new-text');
 
-submitButton.addEventListener('click', function() {
-  newText.innerHTML = '<p>' + ogText.value + '</p>';
+const seeLibButton = document.getElementById('seeLib');
+
+const renderLib = document.getElementById('renderLib');
+
+// submitButton.addEventListener('click', function() {
+//   newText.innerHTML = '<p>' + ogText.value + '</p>';
+//
+// });
+
+seeLibButton.addEventListener('click', function(){
+  let verb = document.getElementById('verb').value;
+  let noun = document.getElementById('noun').value;
+  let adjective = document.getElementById('adjective').value;
+  renderLib.innerHTML = '<p>He ' + verb + ' through the ' + noun + ' while he ate a ' +
+  adjective + ' corndog.</p>';
 
 });
